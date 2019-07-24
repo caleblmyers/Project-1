db93a5d273acce8021c90aab0c0d142ec221f436
Caleb Myers <cmyers5108@gmail.com>
2019-07-24 18:23:26 -0400

    add minor reformatting and refactoring

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,7 @@ var keyword;
 var themeDark = false;
 
 var siteURL = 'https://caleblmyers.github.io/Project-1/'
+
 var openWeatherKey = '280deca1e7bba83d640479281597834f';
 
 //==================================================
@@ -82,10 +83,7 @@ function grabImg(imgArray) {
 	var imgFound = false;
 	var imgUrl;
 	for (let j = 0; j < imgArray.length; j++) {
-		if (
-			imgArray[j].width >= minWidth &&
-			imgArray[j].ratio === '16_9'
-		) {
+		if (imgArray[j].width >= minWidth && imgArray[j].ratio === '16_9') {
 			imgUrl = imgArray[j].url;
 			imgFound = true;
 		}
@@ -99,21 +97,13 @@ function grabImg(imgArray) {
 function mainSearch() {
 	keyword = $('#js-input-search').val().trim();
 
-	// Clears old search results
 	$('#js-results').empty();
 	$('#js-form-search')[0].reset();
 
-	// Checks if user made an input
 	if (keyword === '') {
-		console.log('didnt search');
 		$('#js-display-row').hide();
 	} else {
-		console.log('did search');
-
-		var queryURL =
-			'https://app.ticketmaster.com/discovery/v2/events?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&keyword=' +
-			keyword +
-			'&locale=*&includeSpellcheck=yes';
+		var queryURL = 'https://app.ticketmaster.com/discovery/v2/events?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&keyword=' + keyword + '&locale=*&includeSpellcheck=yes';
 
 		$.ajax({
 			url: queryURL,
@@ -129,128 +119,82 @@ function searchCall(res, key) {
 	console.log(res);
 	console.log(searchData);
 
-	if (searchData === null || searchData === undefined) {
+	if (searchData === undefined) {
 		$('#js-display-header').text('No results found.');
 		$('#js-display-row').show();
-	} else {
-		$('#js-display-header').html(
-			'Showing results for <span class="result__key" id="js-display-input">' +
-			key +
-			'</span>'
-		);
-		console.log(key);
-		$('#js-display-row').show();
+		return
+	}
 
-		if (_.has(res._links.next, 'href')) {
-			searchDataNext = res._links.next.href;
-		} else {
-			searchDataNext = undefined;
+	$('#js-display-header').html('Showing results for <span class="result__key" id="js-display-input">' + key + '</span>');
+	$('#js-display-row').show();
+
+	if (_.has(res._links.next, 'href')) {
+		searchDataNext = res._links.next.href;
+	} else {
+		searchDataNext = undefined;
+	}
+
+	for (let i = 0; i < searchData.events.length; i++) {
+		var classDark = '';
+		if (themeDark) {
+			classDark = 'card--dark';
 		}
 
-		// Creates cards for each matching result
-		for (let i = 0; i < searchData.events.length; i++) {
-			var classDark = '';
-			if (themeDark) {
-				classDark = 'card--dark';
-			}
-			var imgUrl = grabImg(searchData.events[i].images);
-			var eventLat = null;
-			var eventLng = null;
-			var cityName = 'N/A';
-			var stateCode = 'N/A';
-			var venueName = 'Venue Unavailable';
+		var eventObj = searchData.events[i]
 
-			if (_.has(searchData.events[i], 'place')) {
-				eventLat = searchData.events[i].place.location.latitude;
-				eventLng = searchData.events[i].place.location.longitude;
-				cityName = searchData.events[i].place.city.name || 'N/A';
-				if (searchData.events[i].place.countryCode === 'US') {
-					stateCode =
-						searchData.events[i].place.state.stateCode || 'N/A';
+		var imgUrl = grabImg(eventObj.images);
+		var eventLat = null;
+		var eventLng = null;
+		var cityName = 'N/A';
+		var stateCode = 'N/A';
+		var venueName = 'Venue Unavailable';
+
+		if (_.has(eventObj, 'place')) {
+			var eventPlace = eventObj.place
+			eventLat = eventPlace.location.latitude;
+			eventLng = eventPlace.location.longitude;
+			cityName = eventPlace.city.name || 'N/A';
+			if (eventPlace.countryCode === 'US') {
+				stateCode = eventPlace.state.stateCode || 'N/A';
+			} else {
+				stateCode = eventPlace.country.countryCode || 'N/A';
+			}
+		} else if (_.has(eventObj, '_embedded')) {
+			var eventVenue = eventObj._embedded.venues[0]
+			if (_.has(eventVenue, 'location')) {
+				eventLat = eventVenue.location.latitude;
+				eventLng = eventVenue.location.longitude;
+			}
+			if (_.has(eventVenue, 'city')) {
+				cityName = eventVenue.city.name || 'N/A';
+				if (eventVenue.country.countryCode === 'US') {
+					stateCode = eventVenue.state.stateCode || 'N/A';
 				} else {
-					stateCode =
-						searchData.events[i].place.country.countryCode || 'N/A';
-				}
-			} else if (_.has(searchData.events[i], '_embedded')) {
-				if (
-					_.has(searchData.events[i]._embedded.venues[0], 'location')
-				) {
-					eventLat =
-						searchData.events[i]._embedded.venues[0].location
-							.latitude;
-					eventLng =
-						searchData.events[i]._embedded.venues[0].location
-							.longitude;
-				}
-				if (_.has(searchData.events[i]._embedded.venues[0], 'city')) {
-					cityName =
-						searchData.events[i]._embedded.venues[0].city.name ||
-						'N/A';
-					if (
-						searchData.events[i]._embedded.venues[0].country
-							.countryCode === 'US'
-					) {
-						stateCode =
-							searchData.events[i]._embedded.venues[0].state
-								.stateCode || 'N/A';
-					} else {
-						stateCode =
-							searchData.events[i]._embedded.venues[0].country
-								.countryCode || 'N/A';
-					}
+					stateCode = eventVenue.country.countryCode || 'N/A';
 				}
 			}
+		}
 
-			if (_.has(searchData.events[i], '_embedded')) {
-				if (_.has(searchData.events[i]._embedded.venues[0], 'name')) {
-					venueName =
-						searchData.events[i]._embedded.venues[0].name || 'N/A';
-				}
+		if (_.has(eventObj, '_embedded')) {
+			if (_.has(eventObj._embedded.venues[0], 'name')) {
+				venueName = eventObj._embedded.venues[0].name || 'N/A';
 			}
+		}
 
-			console.log('card making running');
-			var newCard = $(
-				"<div class='col-12 col-md-6 col-lg-3'>"
-			).append(
-				$(
-					"<a href='./details.html?id=" +
-					searchData.events[i].id +
-					'&lat=' +
-					eventLat +
-					'&lng=' +
-					eventLng +
-					"' target=_blank class='card-link'>"
-				).append(
-					$(
-						`<div class='card result__card ${classDark} mb-md-3 mb-4'>`
-					).append(
-						$(
-							"<img src='" +
-							imgUrl +
-							"' alt='" +
-							searchData.events[i].name +
-							"' class='card-img-top'>"
-						),
-						$("<div class='card-body'>").append(
-							$("<h5 class='card-title mb-1'>").text(
-								searchData.events[i].name
-							),
-							$("<p class='card-text'>").text(
-								moment(
-									searchData.events[i].dates.start.localDate,
-									'YYYY-MM-DD'
-								).format('MMMM Do YYYY')
-							),
-							$("<p class='card-text mb-0'>").text(venueName),
-							$("<p class='card-text result__info'>").text(
-								cityName + ', ' + stateCode
-							)
-						)
+		var newCard = $("<div class='col-12 col-md-6 col-lg-3'>").append(
+			$("<a href='./details.html?id=" + searchData.events[i].id + '&lat=' + eventLat + '&lng=' + eventLng + "' target=_blank class='card-link'>").append(
+				$(`<div class='card result__card ${classDark} mb-md-3 mb-4'>`).append(
+					$("<img src='" + imgUrl + "' alt='" + searchData.events[i].name + "' class='card-img-top'>"),
+					$("<div class='card-body'>").append(
+						$("<h5 class='card-title mb-1'>").text(searchData.events[i].name),
+						$("<p class='card-text'>").text(moment(searchData.events[i].dates.start.localDate, 'YYYY-MM-DD').format('MMMM Do YYYY')),
+						$("<p class='card-text mb-0'>").text(venueName),
+						$("<p class='card-text result__info'>").text(cityName + ', ' + stateCode)
 					)
 				)
-			);
-			$('#js-results').append(newCard);
-		}
+			)
+		);
+		$('#js-results').append(newCard);
 	}
 }
 
@@ -274,7 +218,6 @@ function checkWindow() {
 // APP INIT
 //--------------------------------------------------
 
-// Gets event details after the user selects an event and moves to details.html
 $(document).ready(function () {
 	checkWindow();
 	window.addEventListener('resize', checkWindow);
@@ -286,48 +229,35 @@ $(document).ready(function () {
 
 	$('#js-toggle-theme').on('change', function () {
 		themeDark = !themeDark;
-		// $(".toggle__switch")
 		transitionTheme();
 		toggleTheme();
 	});
 
-	// Current brower href
 	var pageRef = window.location.href;
 	var currentUrl = new URL(window.location);
 	var searchParams = new URLSearchParams(currentUrl.search);
 
-	// Checks if details.html or index.html is active
 	if (pageRef.search('details') === -1) {
-		console.log('You are on the index page');
-		if (
-			searchParams.get('keyword') !== null &&
-			searchParams.get('keyword') !== undefined
-		) {
+		if (searchParams.get('keyword') !== null && searchParams.get('keyword') !== undefined) {
 			$('#js-input-search').val(searchParams.get('keyword'));
 			mainSearch();
 		}
 	} else {
-		console.log('Details page!!!');
+
 		$('#js-btn-details-search').on('click', function (e) {
 			e.preventDefault();
 			searchDetails();
 		});
 
-		var currentUrl = new URL(window.location);
-		var searchParams = new URLSearchParams(currentUrl.search);
-		console.log(searchParams);
-
 		var detailsID = searchParams.get('id');
-		var queryURL =
-			'https://app.ticketmaster.com/discovery/v2/events/' +
-			detailsID +
-			'?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&locale=*&includeSpellcheck=yes';
+		var queryURL = 'https://app.ticketmaster.com/discovery/v2/events/' + detailsID + '?apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5&locale=*&includeSpellcheck=yes';
 
 		$.ajax({
 			url: queryURL,
 			method: 'GET'
 		}).then(function (res) {
 			console.log(res);
+
 			var imgUrl = grabImg(res.images);
 			$('#js-details-image').attr('src', imgUrl);
 
@@ -338,28 +268,28 @@ $(document).ready(function () {
 			var venueName = 'Venue Unavailable';
 
 			if (_.has(res, 'place')) {
-				cityName = res.place.city.name || 'N/A';
-				if (res.place.countryCode === 'US') {
-					stateCode = res.place.state.stateCode || 'N/A';
+				var place = res.place
+				cityName = place.city.name || 'N/A';
+				if (place.countryCode === 'US') {
+					stateCode = place.state.stateCode || 'N/A';
 				} else {
-					stateCode = res.place.country.countryCode || 'N/A';
+					stateCode = place.country.countryCode || 'N/A';
 				}
-				address = res.place.address.line1 || 'N/A';
-				postalCode = res.place.postalCode || 'N/A';
+				address = place.address.line1 || 'N/A';
+				postalCode = place.postalCode || 'N/A';
 			} else if (_.has(res, '_embedded')) {
-				if (_.has(res._embedded.venues[0], 'city')) {
-					cityName = res._embedded.venues[0].city.name || 'N/A';
-					if (res._embedded.venues[0].country.countryCode === 'US') {
-						stateCode =
-							res._embedded.venues[0].state.stateCode || 'N/A';
+				var venue = res._embedded.venues[0]
+				if (_.has(venue, 'city')) {
+					cityName = venue.city.name || 'N/A';
+					if (venue.country.countryCode === 'US') {
+						stateCode = venue.state.stateCode || 'N/A';
 					} else {
-						stateCode =
-							res._embedded.venues[0].country.countryCode || 'N/A';
+						stateCode = venue.country.countryCode || 'N/A';
 					}
-					postalCode = res._embedded.venues[0].postalCode || 'N/A';
+					postalCode = venue.postalCode || 'N/A';
 				}
-				if (_.has(res._embedded.venues[0], 'address')) {
-					address = res._embedded.venues[0].address.line1 || 'N/A';
+				if (_.has(venue, 'address')) {
+					address = venue.address.line1 || 'N/A';
 				}
 			}
 
@@ -369,26 +299,17 @@ $(document).ready(function () {
 				}
 			}
 
-			// Adds info to the brief section
 			checkForValue(res, 'name', '#js-brief-event');
 			checkForValue(res.dates.start, 'localDate', '#js-brief-date');
 
 			if (_.has(res.dates.start, 'localDate')) {
-				$('#js-brief-date').text(
-					moment(res.dates.start.localDate, 'YYYY-MM-DD').format(
-						'MMMM Do YYYY'
-					)
-				);
+				$('#js-brief-date').text(moment(res.dates.start.localDate, 'YYYY-MM-DD').format('MMMM Do YYYY'));
 			} else {
 				$('#js-brief-date').text('No date available.');
 			}
 
 			if (_.has(res.dates.start, 'localTime')) {
-				$('#js-brief-time').text(
-					moment(res.dates.start.localTime, 'HH:mm:SS').format(
-						'hh:mm A'
-					)
-				);
+				$('#js-brief-time').text(moment(res.dates.start.localTime, 'HH:mm:SS').format('hh:mm A'));
 			} else {
 				$('#js-brief-time').text('No time available.');
 			}
@@ -399,7 +320,6 @@ $(document).ready(function () {
 			$('#js-brief-state').text(stateCode + ' ');
 			$('#js-brief-zip').text(postalCode);
 
-			// Adds info to the details section
 			checkForValue(res, 'name', '#js-details-event');
 			checkForValue(res.dates.start, 'localDate', '#js-details-date');
 			$('#js-details-location').text(venueName);
@@ -407,29 +327,18 @@ $(document).ready(function () {
 			checkForValue(res, 'pleaseNote', '#js-details-note');
 			checkForValue(res, 'info', '#js-details-info');
 
-			//adds link to google maps with venue address
 			if (address === 'N/A') {
 				$('#js-brief-link').attr('href', `#`);
 			} else {
-				$('#js-brief-link').attr(
-					'href',
-					`https://www.google.com/maps?q=${address}+${cityName}+${stateCode}+${postalCode}`
-				);
+				$('#js-brief-link').attr('href', `https://www.google.com/maps?q=${address}+${cityName}+${stateCode}+${postalCode}`);
 			}
 		});
 	}
 });
 
 $(window).scroll(function () {
-	if (
-		$(window).scrollTop() + $(window).height() >
-		$(document).height() - 1 &&
-		searchDataNext !== undefined
-	) {
-		var queryURL =
-			'https://app.ticketmaster.com' +
-			searchDataNext +
-			'&apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5';
+	if ($(window).scrollTop() + $(window).height() > $(document).height() - 1 && searchDataNext) {
+		var queryURL = 'https://app.ticketmaster.com' + searchDataNext + '&apikey=1CDZF2AkHAO8FPwY0r3kQm6bmxI7Vuk5';
 
 		$.ajax({
 			url: queryURL,
